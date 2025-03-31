88d637bd13ed2bc156d8f7d7eef19801fa3edb71
sravan1000 <edu.sravan1000@gmail.com>
2025-03-31 08:26:47 +0530

    test: added failed test case for add of one string value

--- index.spec.ts
+++ index.spec.ts
@@ -4,3 +4,8 @@ import { add } from "./index";
 test('[Add] function call with empty values', () => {
     expect(add()).toStrictEqual(0)
 })
+
+
+test('[Add] function call with a string number value', () => {
+    expect(add("1")).toStrictEqual(1)
+})
