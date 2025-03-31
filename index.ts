8a381365503ececcf93a7ca7c31fddfde8935ab7
sravan1000 <edu.sravan1000@gmail.com>
2025-03-31 11:52:16 +0530

    refactor: format of code

--- index.ts
+++ index.ts
@@ -1,39 +1,45 @@
-export const add = (numbers ?: string): number => {
-    const delimiterRegex = /[,\n]/
-    if(!numbers){
-        return 0
-    }
-    const total = numbers.split(delimiterRegex).reduce((p, c) => p + Number(c), 0)
-    return total
-}
+export const add = (numbers?: string): number => {
+  const delimiterRegex = /[,\n]/;
+  if (!numbers) {
+    return 0;
+  }
+  const total = numbers
+    .split(delimiterRegex)
+    .reduce((p, c) => p + Number(c), 0);
+  return total;
+};
 
-export const getDelimitor = (input ?: string): string => {
-    if(!input){
-        return ""
-    }
-    const conditionRegex = new RegExp(
-        [
-            "^",
-            "//", //  this is for //
-            "(", // start of condition
-            ".*", // any
-            ")" , // end of condition
-            "\\n", // for \n
-            "$" // for end of regex
-        ].join("")
-    )
-    const matches = (input || '').match(conditionRegex);
-    if(!matches) {
-        return ""
-    }
-    const delimitors = matches[1]  // this contains `[` and `]`
-    const delimitorsRegex = new RegExp(
-        [
-            "\\[",
-            "(.*?)", // any
-            "\\]",
-        ].join(""), "g"
-    )
-    const matchedDelimits = [...delimitors.matchAll(delimitorsRegex)].map(m => m[1]); 
-    return matchedDelimits.join("");
-}
+// condition to get delimitor "//[delimiter]\n[numbers…]"
+export const getDelimitor = (input?: string): string => {
+  if (!input) {
+    return "";
+  }
+  const conditionRegex = new RegExp(
+    [
+      "^",
+      "//", //  this is for //
+      "(", // start of condition
+      ".*", // any
+      ")", // end of condition
+      "\\n", // for \n
+      "$", // for end of regex
+    ].join("")
+  );
+  const matches = (input || "").match(conditionRegex);
+  if (!matches) {
+    return "";
+  }
+  const delimitors = matches[1]; // this contains `[` and `]`
+  const delimitorsRegex = new RegExp(
+    [
+      "\\[",
+      "(.*?)", // any
+      "\\]",
+    ].join(""),
+    "g"
+  );
+  const matchedDelimits = [...delimitors.matchAll(delimitorsRegex)].map(
+    (m) => m[1]
+  );
+  return matchedDelimits.join("");
+};
