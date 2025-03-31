810e74ec8e201720fd557090357f578941039cb3
sravan1000 <edu.sravan1000@gmail.com>
2025-03-31 08:38:09 +0530

    refactor: modified code to accept string with two values with , delimitor

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 export const add = (numbers ?: string): number => {
+    const delimiter = ","
     if(!numbers){
         return 0
     }
-    return Number(numbers)
+    const total = numbers.split(delimiter).reduce((p, c) => p + Number(c), 0)
+    return total
 }
