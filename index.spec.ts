ea3dd45d5de242289dce58270be209e50ebb296c
sravan1000 <edu.sravan1000@gmail.com>
2025-03-31 08:33:56 +0530

    test: added failed test to accept string with two values

--- index.spec.ts
+++ index.spec.ts
@@ -9,3 +9,14 @@ test('[Add] function call with empty values', () => {
 test('[Add] function call with a string number value', () => {
     expect(add("1")).toStrictEqual(1)
 })
+
+test('[Add] function call with empty string value', () => {
+    expect(add("")).toStrictEqual(0)
+})
+
+test('[Add] function call with empty string value with spaces in it', () => {
+    expect(add("  ")).toStrictEqual(0)
+})
+test('[Add] function call with a string with two values', () => {
+    expect(add("1,5")).toStrictEqual(6)
+})
