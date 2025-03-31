53ec5dbb1951980d17cb515285f273ce4832d1f5
sravan1000 <edu.sravan1000@gmail.com>
2025-03-31 13:32:00 +0530

    chore: added fn code to satisfy test case

--- helpers/handleNegativeNumbers.ts
+++ helpers/handleNegativeNumbers.ts
@@ -1,3 +1,9 @@
 export const HandleNegativeNumbers = (input: string) => {
-
+    const regex = new RegExp([
+        "-\\d" // negative digits
+    ].join(""), "g")
+    const negatives = input.match(regex) || [];
+    if (negatives?.length) {
+        throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+    }
 }
