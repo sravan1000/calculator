6461185d3bd5bb98c6ff052af5f530a8fadfcb18
sravan1000 <edu.sravan1000@gmail.com>
2025-03-31 13:37:49 +0530

    fix: updated add fn to thow error first when there are negatives

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import { GetDelimitor } from "./helpers/getDelimiter";
+import { HandleNegativeNumbers } from "./helpers/handleNegativeNumbers";
 
 export const add = (input?: string): number => {
   if (!input) {
     return 0;
   }
-  const delimitor = GetDelimitor(input);
   const numbersString = input.replace(/\/\/(.*)\n/, ""); // replace `//[delimit]\n` with ""
+  HandleNegativeNumbers(numbersString)
+  const delimitor = GetDelimitor(input);
   const delimiterRegex = new RegExp(["[", delimitor, "\\n", "]"].join(""));
   const total = numbersString
     .split(delimiterRegex)
